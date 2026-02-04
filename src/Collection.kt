2432b2cec76a9fd02dd9f71b3b27d100922f35fb
vika <vikatekutova7@gmail.com>
2026-02-04 15:02:06 +0300

    Lists in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -7,29 +7,25 @@ val gasPlanets = arrayOf("Jupiter", "Saturn",
 val solarSystem = rockPlanets + gasPlanets
 
 fun main() {
-    println(solarSystem[0])
-    println(solarSystem[1])
-    println(solarSystem[2])
-    println(solarSystem[3])
-    println(solarSystem[4])
-    println(solarSystem[5])
-    println(solarSystem[6])
-    println(solarSystem[7])
-
-    solarSystem[3] = "Little Earth"
-    println(solarSystem[3])
-
-    val newSolarSystem = arrayOf(
-        "Mercury",
-        "Venus",
-        "Earth",
-        "Mars",
-        "Jupiter",
-        "Saturn",
-        "Uranus",
-        "Neptune",
-        "Pluto"
+    val solarSystem = mutableListOf(
+        "Mercury", "Venus", "Earth", "Mars",
+        "Jupiter", "Saturn", "Uranus", "Neptune"
     )
+    println(solarSystem.size)
+    println(solarSystem[2])
+    println(solarSystem.get(3))
+    println(solarSystem.indexOf("Earth"))
+    println(solarSystem.indexOf("Pluto"))
+    for (planet in solarSystem) {
+        println(planet)
+    }
+    solarSystem.add("Pluto")
+    solarSystem.add(3, "Theia")
+    solarSystem[3] = "Future Moon"
+    println(solarSystem[3])
+    println(solarSystem[9])
+    solarSystem.removeAt(9)
+    println(solarSystem.contains("Pluto"))
+    println("Future Moon" in solarSystem)
 
-    println(newSolarSystem[8])
 }
