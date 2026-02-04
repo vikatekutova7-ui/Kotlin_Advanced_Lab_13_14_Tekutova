71bcd387837df5f4f13dffd4fc061b1dd7a569ee
vika <vikatekutova7@gmail.com>
2026-02-04 14:45:03 +0300

    Use scope-functions

--- src/Question.kt
+++ src/Question.kt
@@ -1,48 +1,78 @@
 data class Question<T>(
     val questionText: String,
     val answer: T,
-    val difficulty: Dificulty
+    val difficulty: Difficulty
 )
-enum class Dificulty{
+
+enum class Difficulty {
     EASY,
     MEDIUM,
     HARD
 }
+
 interface ProgressPrintable {
     val progressText: String
     fun printProgressBar()
 }
-class Quiz:ProgressPrintable {
+
+class Quiz : ProgressPrintable {
     override val progressText: String
         get() = "${answered} of ${total} answered"
 
     override fun printProgressBar() {
-        repeat(times = Quiz.answered) { print(" ") }
-        repeat(times = Quiz.total - Quiz.answered) { print(" ") }
+        repeat(answered) { print("▓") }
+        repeat(total - answered) { print("▒") }
         println()
         println(progressText)
     }
+
+    fun printQuiz() {
+        question1.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+
+        question2.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+
+        question3.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+    }
+
     val question1 = Question<String>(
         questionText = "Речка спятила с ума — По домам пошла сама. ___",
         answer = "водопровод",
-        Dificulty.MEDIUM
+        difficulty = Difficulty.MEDIUM
     )
+
     val question2 = Question<Boolean>(
         questionText = "Небо зелёное. Правда или ложь",
         answer = false,
-        Dificulty.EASY
+        difficulty = Difficulty.EASY
     )
+
     val question3 = Question<Int>(
         questionText = "Сколько дней между полнолуниями?",
         answer = 28,
-        Dificulty.HARD
+        difficulty = Difficulty.HARD
     )
-    companion object StudentProgress{
-        var total: Int=10
-        var answered: Int=3
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
     }
 }
 
 fun main() {
-    Quiz().printProgressBar()
+    Quiz().apply { printQuiz() }
 }
